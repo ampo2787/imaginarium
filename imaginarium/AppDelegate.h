f8db1d8dfa8d33e0ee25f1e70418e4503bfbfa95
Your Name <you@your-domain.com>
2018-11-05 17:15:31 +0900

    Initial Commit

---
+++ imaginarium/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  imaginarium
+//
+//  Created by JihoonPark on 05/11/2018.
+//  Copyright © 2018 JihoonPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ imaginarium/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  imaginarium
+//
+//  Created by JihoonPark on 05/11/2018.
+//  Copyright © 2018 JihoonPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
